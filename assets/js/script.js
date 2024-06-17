82c7f0c480c2459560c8d634f18ab27b3f2b7cc3
unknown <maximilianoacri1@gmail.com>
2024-06-17 00:57:40 -0300

    todo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const text = "AI Development Company";
-    let index = 0;
+    const titles = ["AI Development Company", "Innovative AI Solutions", "AI Technology Experts"];
+    let titleIndex = 0;
+    let charIndex = 0;
     const speed = 80;  // Velocidad de la escritura (ms)
+    const delayBetweenTitles = 1500;  // Retardo entre títulos (ms)
 
     function typeWriter() {
-        if (index < text.length) {
-            document.getElementById("typewriter-title").innerHTML += text.charAt(index);
-            index++;
+        if (charIndex < titles[titleIndex].length) {
+            document.getElementById("typewriter-title").innerHTML += titles[titleIndex].charAt(charIndex);
+            charIndex++;
             setTimeout(typeWriter, speed);
+        } else {
+            setTimeout(() => {
+                charIndex = 0;
+                titleIndex = (titleIndex + 1) % titles.length;
+                document.getElementById("typewriter-title").innerHTML = "";
+                typeWriter();
+            }, delayBetweenTitles);
         }
     }
 
