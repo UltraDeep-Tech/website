c7d03169ed692dc5bdbfe133601328a2f750f002
unknown <maximilianoacri1@gmail.com>
2024-06-26 15:46:05 -0300

    todo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const titles = ["AI Development Company", "Innovative AI Solutions", "AI Technology Experts","AI Safety & Security"];
+    const titles = ["AI Development Company", "Innovative AI Solutions", "AI Technology Experts", "AI Safety & Security"];
     let titleIndex = 0;
     let charIndex = 0;
     const speed = 80;  // Velocidad de la escritura (ms)
     const delayBetweenTitles = 1500;  // Retardo entre títulos (ms)
+    const typewriterTitle = document.getElementById("typewriter-title");
 
     function typeWriter() {
         if (charIndex < titles[titleIndex].length) {
-            document.getElementById("typewriter-title").innerHTML += titles[titleIndex].charAt(charIndex);
+            typewriterTitle.innerHTML += titles[titleIndex].charAt(charIndex);
             charIndex++;
             setTimeout(typeWriter, speed);
         } else {
             setTimeout(() => {
                 charIndex = 0;
                 titleIndex = (titleIndex + 1) % titles.length;
-                document.getElementById("typewriter-title").innerHTML = "";
+                typewriterTitle.innerHTML = "";
                 typeWriter();
             }, delayBetweenTitles);
         }
     }
 
     typeWriter();
-});
 
-document.addEventListener('DOMContentLoaded', () => {
     const sitename = document.querySelector('.sitename');
     const logoImg = document.querySelector('.logo-img');
     
     sitename.classList.add('glow');
     logoImg.classList.add('glow');
 });
+
 
 
 
