89605fb818cf44b2ddfacb0380de78017d7a3b53
unknown <maximilianoacri1@gmail.com>
2024-07-08 19:36:11 -0300

    todo

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Configurar la ruta de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para el archivo index.html
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
