845877a7269a4efe2d7701c868e56289199544e4
unknown <maximilianoacri1@gmail.com>
2024-09-18 21:25:22 -0300

    todo

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -2,32 +2,38 @@ const API_KEY = 'AIzaSyCYoJ0hGQLahZkYGaJsbk9N2wyj14K80Jw';
 const CHANNEL_ID = 'UCivpA1Q4U8Y09Kvp1_j5QRg';
 const API_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=5`;
 
+// Function to fetch videos and add them to Swiper
 fetch(API_URL)
   .then(response => response.json())
   .then(data => {
     const videos = data.items;
-    let videoContainer = document.getElementById('video-container');
-    
+    const videoContainer = document.getElementById('video-container');
+
     videos.forEach(video => {
       if (video.id.kind === 'youtube#video') {
         let videoId = video.id.videoId;
         let videoTitle = video.snippet.title;
 
-        let slide = document.createElement('div');
-        slide.classList.add('swiper-slide');
-        slide.innerHTML = `
-          <div class="video-thumbnail">
-            <iframe width="100%" height="315" src="https://www.youtube.com/embed/${videoId}" title="${videoTitle}" frameborder="0" allowfullscreen></iframe>
-            <p>${videoTitle}</p>
+        // Create a Swiper slide with the video
+        let videoSlide = document.createElement('div');
+        videoSlide.classList.add('swiper-slide');
+        videoSlide.innerHTML = `
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="${videoTitle}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
+          <p>${videoTitle}</p>
         `;
-        videoContainer.appendChild(slide);
+        videoContainer.appendChild(videoSlide);
       }
     });
 
-    // Initialize Swiper.js after dynamic content is added
-    new Swiper('.swiper-container', {
+    // Initialize Swiper after videos are loaded
+    const swiper = new Swiper('.swiper-container', {
       loop: true,
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 1,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -38,4 +44,4 @@ fetch(API_URL)
       },
     });
   })
-  .catch(error => console.log('Error fetching YouTube videos:', error));
+  .catch(error => console.error('Error fetching YouTube videos:', error));
