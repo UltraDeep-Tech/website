3f0d873163eae9419ab92222bc243426e1ac715b
unknown <maximilianoacri1@gmail.com>
2025-03-18 17:57:24 -0300

    se actualizo la pagina considerablemente, se borro ccomerce y se mejoro el aspecto visual y se quito el video

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,16 +111,7 @@
     selector: '.glightbox'
   });
 
-  /**
-   * Init swiper sliders
-   */
-  function initSwiper() {
-    document.querySelectorAll('.swiper').forEach(function(swiper) {
-      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
-      new Swiper(swiper, config);
-    });
-  }
-  window.addEventListener('load', initSwiper);
+
 
   /**
    * Init isotope layout and filters
